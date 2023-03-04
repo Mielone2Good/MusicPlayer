78237599c43b83f3fa4bbb2d925471e534dae09c
Mielony <ziemniak@mielone.pl>
2023-03-04 20:47:56 +0100

    added volume settings, some code fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 import pygame.mixer as mixer
 import os
 
+
 songsNum = 0
 kPiosenka = 0
 songs = []
@@ -16,16 +17,18 @@ root.title('Music Player')
 root.resizable(0, 0)
 root.configure(bg="#303030")
 
+
 songStopped = 1
 
 
+volu = Label(root,text="Volume",bg="#303030",fg="#909191",bd="0px",font=("Fantasy", "10", "bold italic")).place(x=10,y=190)
 
 
 
-songsLab = tkinter.Text(root,width=35,height=16,relief=RIDGE,bg="deepskyblue")
+songsLab = tkinter.Text(root,width=35,fg="white",height=16,relief=RIDGE,bg="#383a3b")
 songsLab.place(x=450, y=10)
 
-print(type(songsLab))
+
 
 def zaladuj(listbox):
     global Loaded
@@ -34,7 +37,6 @@ def zaladuj(listbox):
     songsNum = 0
     os.chdir(filedialog.askdirectory(title='Open a songs directory'))
     tracks = os.listdir()
-    print(tracks)
     for track in tracks:
             listbox.append(track)
     for song in songs:
@@ -51,7 +53,7 @@ def stopp_song():
     SongName["text"]=songs[kPiosenka]
     if Loaded == False:
         mixer.music.load(songs[0])
-        mixer.music.play()
+        mixer.music.play(-1)
         mixer.music.unpause()
 
         Loaded = True
@@ -70,21 +72,32 @@ def next_song():
     kPiosenka += 1
     SongName["text"] = songs[kPiosenka]
     mixer.music.load(songs[kPiosenka])
-    mixer.music.play()
+    mixer.music.play(-1)
     mixer.music.unpause()
+
 
 def previous_song():
     global kPiosenka
     kPiosenka -= 1
     SongName["text"] = songs[kPiosenka]
     mixer.music.load(songs[kPiosenka])
-    mixer.music.play()
+    mixer.music.play(-1)
     mixer.music.unpause()
 
-
+def zmien_g(_=None):
+    vol = var.get()
+    vol = vol/10
+    vol = vol/10
+    mixer.music.set_volume(vol)
 
 def load_song():
     zaladuj(songs)
+
+var = DoubleVar()
+
+
+
+
 
 
 
@@ -95,11 +108,12 @@ stop.place(x=170,y=230)
 nextSong = Button(root,text="⏭",bg="#303030",relief=GROOVE,activebackground="#303030",activeforeground="#aeb0b0",bd="0px",fg="#aeb0b0",font=("Fantasy", "15", "bold"),command=next_song).place(x=250,y=250)
 previousSong = Button(root,text="⏮",bg="#303030",relief=GROOVE,activebackground="#303030",activeforeground="#aeb0b0",bd="0px",fg="#aeb0b0",font=("Fantasy", "15", "bold"),command=previous_song).place(x=110,y=250)
 SongName = Label(root,text="song name",bg="#303030",relief=GROOVE,width=25,bd="1px",fg="#aeb0b0",font=("Fantasy", "15", "bold italic"))
-SongName.place(x=50,y=210)
+SongName.place(x=80,y=210)
 
+suwak = tkinter.Scale(root,from_=10,to=1,orient="vertical",bd="0px",width=15,highlightcolor="#4a5052",command=zmien_g,activebackground="#4a5052",highlightbackground="#525454",showvalue=0,troughcolor="#4a5052",bg="#303030",variable=var)
+suwak.place(x=25,y=210)
 
-
-
+suwak.set(10)
 
 
 root.mainloop()
